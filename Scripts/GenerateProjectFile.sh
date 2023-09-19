976af3600b9594544d3f9a742f5e19f737fbbd34
Sebastián Turén <sebaturen@gmail.com>
2023-09-19 11:33:23 -0300

    Added mac project build and update project dependencies

---
+++ Scripts/GenerateProjectFile.sh
@@ -0,0 +1,1 @@
+/Users/cleinother/Epic/UE_5.2/Engine/Build/BatchFiles/Mac/GenerateProjectFiles.sh -project=/Users/cleinother/Documents/GitHub/UnrealDK/UnrealDK/UnrealDK.uproject -game -vscode
--- UnrealDK/Source/UnrealDK/UnrealDK.Build.cs
+++ UnrealDK/Source/UnrealDK/UnrealDK.Build.cs
@@ -8,7 +8,13 @@ public class UnrealDK : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore",
+			"PhysicsCore" 
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
